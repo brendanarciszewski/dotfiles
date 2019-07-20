0b1249e3e3af1aa7937149905299cef5e7264ee5
Brendan Arciszewski <arciszewski.bd@gmail.com>
2019-07-20 14:03:37 -0400

    smaller diffs, remove used programs

--- install-scripts/pkg-install.sh
+++ install-scripts/pkg-install.sh
@@ -1,27 +1,73 @@
 xcode-select --install
-brew install zsh #get the latest
-brew install yarn python3 sympy go coreutils octave neovim
-brew install ruby --with-doc
+brew install zsh
+brew install\
+    python3\
+    go\
+    octave\
+    rust\
+
+brew install\
+    yarn\
+    sympy\
+    coreutils\
+    neovim\
+    wget\
+    htop\
+    nmap\
+    geoip\
+    irssi\
+    watch\
+
 brew install sleepwatcher #reconfigure sleepwatcher config to point to binary
-brew install wget htop nmap geoip irssi watch
 brew services start sleepwatcher
 
 brew tap caskroom/cask
-brew cask install master-password alfred malwarebytes coconutbattery
-brew cask install google-chrome firefox torbrowser transmission
-brew cask install handbrake dropbox google-backup-and-sync onedrive
-brew cask install github-desktop gitkraken arduino sublime-text #install ‘Six', 'LaTeXTools', 'LaTeXTab', 'Floobits', 'Print to HTML'
-brew cask install mactex texstudio
-brew cask install skype slack discord iina spotify
-brew cask install copyq etcher the-unarchiver wireshark imageoptim cheatsheet #get McGimp instead of gimp
-brew cask install alacritty intellij-idea clion clang-format java anki
+#Editors
+brew cask install\
+    intellij-idea\
+    clion\
+    mactex\
+    texstudio\
+    sublime-text #install ‘Six', 'LaTeXTools', 'LaTeXTab', 'Floobits', 'Print to HTML'
 
+#Terminal & Tools
+brew cask install\
+    alacritty\
+    gitkraken\
+    arduino\
+    clang-format\
+    java\
+
+#Generic Apps
+brew cask install\
+    anki\
+    master-password\
+    alfred\
+    malwarebytes\
+    coconutbattery\
+    google-chrome\
+    firefox\
+    transmission\
+    handbrake\
+    dropbox\
+    google-backup-and-sync\
+    onedrive\
+    skype\
+    slack\
+    discord\
+    iina\
+    spotify\
+    copyq\
+    etcher\
+    the-unarchiver\
+    wireshark\
+    imageoptim\
+    cheatsheet\
+    #get McGimp instead of gimp
+
+#Filesystem Drivers
 brew cask install osxfuse
 brew install sshfs e2fsprogs #then download https://github.com/gpz500/fuse-ext2/releases
-
-brew tap bazelbuild/tap
-brew tap-pin bazelbuild/tap
-brew install bazel
 
 
 gem install jekyll bundler
